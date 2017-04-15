6a18a18c7c6f01e5d2fc0335ac8faa1ebf4b5ca5
Tommy H. Yeargin, Jr <tommy.yeargin@gmail.com>
2017-04-14 23:38:25 -0400

    commit

---
+++ 01_working_with_lists.py
@@ -0,0 +1,8 @@
+# spam1 = [['cat', 'bat'], ['beer', 'deer', 'gear']]
+# print(spam1)
+# spam1[0] = spam1[0] + ['fat']
+# print(spam1)
+# print(spam1[0][2])
+# print((spam1[0][2] + ' ass ') * 10 + spam1[1][1])
+for i in range(5):
+    print(i + 1)
